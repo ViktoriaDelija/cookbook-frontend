6c3edc1cabff8b6102ece76294e3d62a760e0f72
Viktoria Delija <viktoriad3@gmail.com>
2023-09-13 18:13:00 +0200

    Added ingredient details

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Home = () => {
-  return <div>Welcome!</div>;
+  return (
+    <main>
+      <h1>Welcome!</h1>
+      <Link to="/ingredients">Ingredients</Link>
+    </main>
+  );
 };
 
 export default Home;
--- src/IngredientListItem.js
+++ src/IngredientListItem.js
@@ -1,10 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import IngredientDetails from "./IngredientDetails";
 
 const IngredientListItem = ({ ingredient, ingredients, setIngredients }) => {
   return (
-    <li className="ingredient" key={ingredient.ID}>
-      {ingredient.name}
-    </li>
+    <article className="ingredient" key={ingredient.id}>
+      <Link to={`/ingredients/${ingredient.id}`}> {ingredient.name}</Link>
+    </article>
   );
 };
 
