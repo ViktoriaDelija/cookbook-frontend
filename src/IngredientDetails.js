928d306fe4448d86459e58db9f593b383b17da8b
Viktoria Delija <viktoriad3@gmail.com>
2023-09-14 13:38:22 +0200

    Added delete ingredient

--- src/IngredientDetails.js
+++ src/IngredientDetails.js
@@ -1,18 +1,18 @@
 import React, { useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import axios from "axios";
 import { useState } from "react";
 
-const IngredientDetails = ({ ingredients }) => {
+const IngredientDetails = ({ handleDelete, ingredient, setIngredient }) => {
   const API_URL = "http://localhost:8080/api/ingredients";
   const { ingId } = useParams();
-  const [ingredient, setIngredient] = useState({});
 
   useEffect(() => {
     const fetchIngredient = async () => {
       try {
         const response = await axios.get(API_URL + `/${ingId}`);
         setIngredient(response.data);
+        console.log(ingredient.id);
       } catch (error) {
         console.log(error.response.data);
         console.log(error.response.status);
@@ -28,6 +28,9 @@ const IngredientDetails = ({ ingredients }) => {
         <p>{ingredient.description}</p>
         <p>{ingredient.ingType}</p>
         <p>{ingredient.price} €</p>
+        <button onClick={() => handleDelete(ingredient.id)}>
+          Delete ingredient
+        </button>
       </article>
     </main>
   );
